0a67f9e2e7089cb2c8116d8ee4e03710484813d1
Alexander Marchenko <maappdev@gmail.com>
2014-08-09 16:36:54 +0300

    Create base rander with checkbox and color circle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,24 +10,37 @@
  */
 angular
   .module('ngselectCustomizeApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ui.select2'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .run(['$rootScope', function($rootScope){
+    $rootScope.select2Status = {
+      items:[
+        {name: 'Paid', id: 1, color:'red', checked: false},
+        {name: 'Guest', id: 2, color:'green', checked: false},
+        {name: 'Hot List', id: 3, color: 'blue', checked: false}
+      ]
+    };
+    var itemRenderResult = function(item){
+      var currentItem = null;
+      if(item.id){
+        currentItem = $rootScope.select2Status.items[item.id-1];
+      }
+      return '<input type="checkbox" '+(currentItem && currentItem.checked ? 'checked="checked"' : '')+' />'+
+      '<div class="circle '+currentItem.color+'"></div>' + item.text;
+    };
+    var itemRenderSelection = function(item){
+      var selectedItem = null;
+      if(item.id){
+        selectedItem = $rootScope.select2Status.items[item.id-1];
+        selectedItem.checked = true;
+        //@todo: make checkbox unchecked
+      };
+      return '<input type="checkbox" checked="checked"/>' + item.text;
+    }
+    $rootScope.select2Options = {
+      minimumResultsForSearch: '-1',
+      'width': '250px',
+      formatResult: itemRenderResult,
+      formatSelection: itemRenderSelection,
+    };
+  }]);
