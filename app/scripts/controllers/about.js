a3ca19de9beea941d831a701d5e030a61534e663
Alexander Marchenko <maappdev@gmail.com>
2014-08-09 11:02:13 +0300

    Init ui-select2 repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngselectCustomizeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngselectCustomizeApp
+ */
+angular.module('ngselectCustomizeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
